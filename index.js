227d41b2bae69cddf28b9fceb826e808c12da9d9
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-09-28 12:19:16 +0100

    commented a code

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 
-const { getHTML, getTwitterFollowers } = require ('./lib/scrapper.js');
+const { getHTML, getTwitterFollowers, getInstagramFollowers } = require ('./lib/scrapper.js');
 
 async function go(){
-    const html = await getHTML('https://www.twitter.com/wesbos');
-    const twCount = await getTwitterFollowers(html);
-    console.log(`You have ${twCount} followers`);
+    // const html = await getHTML('https://www.twitter.com/wesbos');
+    // const twCount = await getTwitterFollowers(html);
+    // console.log(`You have ${twCount} followers`);
+
+
 }
 
 go();
